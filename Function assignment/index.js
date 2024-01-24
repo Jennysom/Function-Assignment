7c16517431ec0451157dce6f4639a2ec55e00039
Jennysom <152967613+Jennysom@users.noreply.github.com>
2024-01-25 02:52:14 +1300

    Add files via upload

---
+++ Function assignment/index.js
@@ -0,0 +1,11 @@
+function checkUpto(age) {
+    if(age>30) {
+        return true;
+    }else 
+    return false;
+}
+
+const personAge = 35;
+const result = checkUpto(personAge)
+
+console.log(result)
